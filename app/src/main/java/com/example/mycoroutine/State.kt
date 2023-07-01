6d58f3853a004125362b86b4d7720ce02758652b
VladJu <juikovvlad@gmail.com>
2023-07-01 12:42:09 +0300

    Sealed Classes

--- app/src/main/java/com/example/mycoroutine/State.kt
+++ app/src/main/java/com/example/mycoroutine/State.kt
@@ -1,8 +1,13 @@
 package com.example.mycoroutine
 
+
+sealed class State
+
 //1
-class State(
-    val isError : Boolean = false,
-    val isInProgress : Boolean = false,
-    val factorial : String = ""
-)
+//чтобы каждый раз не создавать новый object, будем использовать 1 и тот же объект
+object Error : State()
+object Progress : State()
+//Не сможем сделать объектом т.к у него есть поле и надо будет каждый раз создавать новый экземлр
+class Result(
+    val factorial: String
+) : State()
